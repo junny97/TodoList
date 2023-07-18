da15eca4a5d22117e15debd7bc75ea358b37bd65
junny97 <pcohad12@gmail.com>
2023-07-19 00:45:06 +0900

    [Feat] 날씨 상태에 맞는 날씨 이모지 추가하는 기능 구현

--- js/weather.js
+++ js/weather.js
@@ -6,15 +6,36 @@ function onGeoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector('#weather span:first-child');
-      const city = document.querySelector('#weather span:last-child');
+      const weatherIcon = document.getElementById('weather-icon');
+      const weatherText = document.getElementById('weather-text');
+      const city = document.getElementById('city');
       city.innerText = data.name;
-      weather.innerText = data.weather[0].main;
+      weatherText.innerText = data.weather[0].main;
+      city.innerText = data.name;
+
+      const weatherIcons = {
+        Clear: '☀️',
+        Clouds: '☁️',
+        Rain: '🌧️',
+        Drizzle: '🌦️',
+        Thunderstorm: '⛈️',
+        Snow: '❄️',
+        Mist: '🌫️',
+        Smoke: '🌫️',
+        Haze: '🌫️',
+        Dust: '🌫️',
+        Fog: '🌫️',
+        Squall: '🌪️',
+        Tornado: '🌪️🌪️',
+      };
+
+      // 날씨 이모지 추가
+      weatherIcon.innerHTML = weatherIcons[data.weather[0].main] || '';
     });
 }
 
 function onGeoError() {
-  alert("Can't find you. No weather for you.");
+  alert('위치를 찾을 수 없습니다. 위치 설정을 먼저 해주세요');
 }
 //위치를 나타낼때는 getCurrentPosition함수 내에 위치 성공함수와 실패함수를 정의해서 넣어야 함
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
