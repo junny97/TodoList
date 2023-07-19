263a6174dc177fa3caffa5ba241ab9822cc281bb
junny97 <pcohad12@gmail.com>
2023-07-19 14:41:46 +0900

    [Feat] TodoList 내용 수정 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -8,39 +8,66 @@ let toDos = [];
 function savetoDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
-  // toDo.id는 문자열이고 li.id는 스트링이라 삭제되지 않음 삭제하려면 parseint로 문자열로 전환
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   savetoDos();
 }
+
+function editToDo(event) {
+  const li = event.target.parentElement;
+  const todoText = li.querySelector('span').innerText;
+  const newText = prompt('Edit the to-do item:', todoText);
+
+  if (newText !== null) {
+    if (newText.trim() !== '') {
+      // Check if the newText is not empty or contains only spaces
+      li.querySelector('span').innerText = newText;
+      const todoId = parseInt(li.id);
+      toDos = toDos.map((todo) => {
+        if (todo.id === todoId) {
+          return { ...todo, text: newText };
+        }
+        return todo;
+      });
+      savetoDos();
+    } else {
+      alert('오늘의 할 일 수정사항을 입력해 주세요!');
+    }
+  }
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   li.id = newTodo.id;
+  li.classList.add('todo-item');
+
   const span = document.createElement('span');
   span.innerText = newTodo.text;
-  const button = document.createElement('button');
-  button.innerText = 'X';
-  button.addEventListener('click', deleteToDo);
+
+  const editButton = document.createElement('button');
+  editButton.innerText = 'Edit';
+  editButton.classList.add('todo-button');
+  editButton.addEventListener('click', editToDo);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'X';
+  deleteButton.classList.add('todo-button');
+  deleteButton.addEventListener('click', deleteToDo);
+
   li.appendChild(span);
-  li.appendChild(button);
+  li.appendChild(editButton);
+  li.appendChild(deleteButton);
+
   toDoList.appendChild(li);
-  li.style.color = 'white';
-  li.style.margin = '20px';
-  button.style.fontSize = '20px';
-  button.style.backgroundColor = '#6c757d';
-  button.style.color = 'white';
-  button.style.border = 'none';
-  button.style.padding = '8px 16px';
-  button.style.borderRadius = '4px';
-  button.style.cursor = 'pointer';
 }
 
 function handleToDoSubimt(event) {
   event.preventDefault();
-  const newTodo = toDoInput.value; //input의 값을 새로운 변수 newTodo에 저장
-  toDoInput.value = ''; //newTodo에 저장한 값은 새로운 변수값이라 벨류값이 사라져도 문제없ㄷ음
+  const newTodo = toDoInput.value;
+  toDoInput.value = '';
   const newTodoObj = {
     text: newTodo,
     id: Date.now(),
@@ -53,10 +80,10 @@ function handleToDoSubimt(event) {
 
 toDoForm.addEventListener('submit', handleToDoSubimt);
 
-const saveToDos = localStorage.getItem(TODOS_KEY);
-console.log(saveToDos);
-if (saveToDos !== null) {
-  const parsedToDos = JSON.parse(saveToDos);
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  parsedToDos.forEach(paintToDo); // forEach는 각각의 배열을 실행시킴
+  parsedToDos.forEach(paintToDo);
 }
